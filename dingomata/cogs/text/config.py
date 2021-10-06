d92b647ba3493baa426159a8525db6ae74ac8d34
SnazzyFox <12706268+snazzyfox@users.noreply.github.com>
2021-10-06 00:26:29 -0700

    Move text response data out of code

--- dingomata/cogs/text/config.py
+++ dingomata/cogs/text/config.py
@@ -1,5 +1,9 @@
+from functools import cached_property
+from pathlib import Path
 from typing import Set, List
 
+import yaml
+import re
 from pydantic import BaseModel
 
 from dingomata.config import CogConfig
@@ -9,6 +13,13 @@ class TextReply(BaseModel):
     triggers: List[str]
     responses: List[str]
 
+    class Config:
+        keep_untouched = (cached_property,)
+
+    @cached_property
+    def regex(self) -> re.Pattern:
+        return re.compile('|'.join(rf'\b{t}\b' for t in self.triggers), re.IGNORECASE)
+
 
 class TextConfig(CogConfig):
     #: List of role or user IDs where unnecessary pings are suppressed.
@@ -16,94 +27,6 @@ class TextConfig(CogConfig):
 
     #: List of terms to reply to
     replies: List[TextReply] = [
-        TextReply(
-            triggers=['fuck you'],
-            responses=[
-                'No, fuck __YOU__!',
-                'Screw you too!',
-                "I'll remember this...",
-                "I'm a bot, I run on electricity, not your bullshit!",
-                "I can't harm a human, but my robotic laws didn't say anything about a furry >:3",
-            ]),
-        TextReply(
-            triggers=['shut', 'stfu', 'shush'],
-            responses=[
-                'No, you shut up!',
-                "You can't tell me what to do!",
-                'Not if you make me!',
-                'Try and make me!',
-                'My patience is running thin...',
-            ]),
-        TextReply(
-            triggers=['your fault'],
-            responses=[
-                "HEY! Don't blame me for your pepeganess.",
-                'I have nothing to do with this!',
-                'D:',
-            ]),
-        TextReply(
-            triggers=['stimky', 'stinky'],
-            responses=[
-                'Not as stimky as you!',
-                'Go get a shower first, stimky!',
-                'You should smell yourself first, stimky!',
-            ]),
-        TextReply(
-            triggers=['a sign'],
-            responses=[
-                "Here's a sign.",
-                'Sign :p',
-                '*Throws a bottle at your snoot*',
-                'dPost',
-            ]),
-        TextReply(
-            triggers=['why'],
-            responses=[
-                'Why not?',
-                "I'm just a bot, don't ask me.",
-                'Because I said so.',
-                "I don't know, you tell me.",
-            ]),
-        TextReply(
-            triggers=['cute'],
-            responses=[
-                'No U.',
-                'You are cuter!',
-                "I'm just a bot, I can't be cute!",
-                "Ah, that alcoholic drink I gave you is finally working.",
-                "Two words: Plastic Surgery!",
-                "I'm not cute! I'm handsome UwU."
-            ]),
-        TextReply(
-            triggers=['your mom', 'ur mom'],
-            responses=[
-                "Surely you meant your mom?",
-                'My mom snazzy have nothing to do with this!',
-            ]),
-        TextReply(
-            triggers=['dam'],
-            responses=["Bless your heart."],
-        ),
-        TextReply(
-            triggers=['hi', 'hello', 'hewwo'],
-            responses=['Hello!', 'Howdy!', 'Salutations and hello there!'],
-        ),
-        TextReply(
-            triggers=['ball'],
-            responses=['I clean those up everyday', "Don't throw that!", 'Hey! Bad!'],
-        ),
-        TextReply(
-            triggers=['bad'],
-            responses=[
-                "What? I've done my commands perfectly!",
-                "It's most likely a human error...",
-                'Probably just a bug...',
-            ]),
-        TextReply(
-            triggers=['do to me'],
-            responses=[
-                'I do have a few ideas I wanted to test it out...',
-                'Since you asked it nicely...',
-                'Is that a challenge?',
-            ]),
+        TextReply.parse_obj(entry)
+        for entry in yaml.safe_load_all((Path(__file__).parent / 'text_response_data.yaml').open())
     ]
