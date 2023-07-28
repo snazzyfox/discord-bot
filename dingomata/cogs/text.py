7d4495e140ddb3046f431daf3585a953c2776699
SnazzyFox <12706268+snazzyfox@users.noreply.github.com>
2023-07-27 20:32:08 -0700

    random temperature owo

--- dingomata/cogs/text.py
+++ dingomata/cogs/text.py
@@ -368,7 +368,7 @@ class TextCog(BaseCog):
         response = await openai.ChatCompletion.acreate(
             model='gpt-3.5-turbo-0613',
             messages=messages,
-            temperature=1.2,
+            temperature=random.betavariate(2, 3) * 2,
             max_tokens=120,
             presence_penalty=0.05,
             frequency_penalty=0.10,
