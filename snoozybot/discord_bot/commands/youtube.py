e55136cd408ac1c2466f1e8474cd44a932e9ded6
snazzyfox <12706268+snazzyfox@users.noreply.github.com>
2024-08-05 10:09:08 -0700

    make youtube notifs check entire playlist not just top video

--- snoozybot/discord_bot/commands/youtube.py
+++ snoozybot/discord_bot/commands/youtube.py
@@ -13,7 +13,7 @@ from snoozybot.config.provider import get_secret_configs
 from snoozybot.utils import LightbulbPlugin
 
 plugin = LightbulbPlugin('youtube')
-_LAST_KNOWN_VIDEO: dict[str, str] = {}
+_KNOWN_PLAYLIST_VIDEOS: dict[str, set[str]] = {}
 logger = logging.getLogger(__name__)
 
 
@@ -25,23 +25,27 @@ class Youtube:
     def set_api_key(self, key: str) -> None:
         self.__api_key = key
 
-    async def get_latest_video(self, playlist_id: str) -> tuple[str, str, str] | None:
-        async with self._client.get('https://youtube.googleapis.com/youtube/v3/playlistItems', params={
-            'part': 'snippet',
-            'playlistId': playlist_id,
-            'maxResults': 1,
-            'key': self.__api_key,
+    async def _get_youtube_api(self, resource: str, params: dict) -> dict:
+        async with self._client.get('https://youtube.googleapis.com/youtube/v3/' + resource, params=params, headers={
+            'X-Goog-Api-Key': self.__api_key,
         }) as resp:
             resp.raise_for_status()
-            data = await resp.json()
-            items = data['items']
-            if not items:
-                return None
-            return (
-                items[0]['snippet']['channelTitle'],
-                items[0]['snippet']['resourceId']['videoId'],
-                items[0]['snippet']['publishedAt'],
-            )
+            return await resp.json()
+
+    async def get_all_playlist_videos(self, playlist_id: str) -> list[str]:
+        ids: list[str] = []
+        page_token: str | None = ''
+        while page_token is not None:
+            data = await self._get_youtube_api('playlistItems', {
+                'part': 'id', 'playlistId': playlist_id, 'maxResults': 50})
+            ids.extend(item['id'] for item in data['items'])
+            page_token = data.get('nextPageToken')
+        return ids
+
+    async def get_playlist_video_details(self, playlist_item_ids: list[str]) -> list[tuple[str, str]]:
+        data = await self._get_youtube_api('playlistItems', {
+            'part': 'snippet', 'id': ','.join(playlist_item_ids), 'maxResults': 50})
+        return [(item['snippet']['channelTitle'], item['snippet']['resourceId']['videoId']) for item in data['items']]
 
     async def close(self):
         await self._client.close()
@@ -69,32 +73,32 @@ async def on_stopped(event: hikari.StoppedEvent):
 
 
 async def _check_and_notify_youtube(playlist_id: str, guilds: set[int], app: lightbulb.BotApp):
-    video = await youtube.get_latest_video(playlist_id)
-    if not video:
-        return
-    video_channel, video_id, timestamp = video
-    if playlist_id not in _LAST_KNOWN_VIDEO:
-        # New video
-        _LAST_KNOWN_VIDEO[playlist_id] = timestamp
-        logger.info('Found initial video %s for playlist %s', video_id, playlist_id)
-    elif _LAST_KNOWN_VIDEO[playlist_id] < timestamp:
-        # New video to notify
-        logger.info('Found new youtube video %s in playlist %s, sending notifications...', video_id, playlist_id)
-        _LAST_KNOWN_VIDEO[playlist_id] = timestamp
-        for guild in guilds:
-            channel_id = await values.youtube_notif_channel_id.get_value(guild)
-            channel = app.cache.get_guild_channel(channel_id)
-            if isinstance(channel, hikari.TextableChannel):
-                content_template = await values.youtube_notif_title_template.get_value(guild) or ''
-                video_url = 'https://youtu.be/' + video_id
-                content = string.Template(content_template).safe_substitute({
-                    'channel': video_channel, 'url': video_url,
-                })
-                await channel.send(content=content, user_mentions=True, role_mentions=True, mentions_everyone=True)
-                logger.info('Sent youtube notification to guild %s channel %s', guild, channel_id)
-            else:
-                logger.error('Did not send a youtube notification for guild %s channel %s: invalid channel ID',
-                             guild, channel)
+    logger.info('Checking new youtube videos for guild %s, playlist %s', guilds, playlist_id)
+    all_videos: set[str] = set(await youtube.get_all_playlist_videos(playlist_id))
+    if playlist_id not in _KNOWN_PLAYLIST_VIDEOS:
+        # First load of playlist; keep for later reference
+        _KNOWN_PLAYLIST_VIDEOS[playlist_id] = all_videos
+        logger.info('Initial load: %d videos in playlist %s', len(all_videos), playlist_id)
+    elif new_videos := all_videos - _KNOWN_PLAYLIST_VIDEOS[playlist_id]:
+        # There are new videos to notify
+        logger.info('Found new youtube videos %s in playlist %s, sending notifications...', new_videos, playlist_id)
+        _KNOWN_PLAYLIST_VIDEOS[playlist_id] = all_videos
+
+        for video_channel, video_id in await youtube.get_playlist_video_details(list(new_videos)):
+            for guild in guilds:
+                channel_id = await values.youtube_notif_channel_id.get_value(guild)
+                channel = app.cache.get_guild_channel(channel_id)
+                if isinstance(channel, hikari.TextableChannel):
+                    content_template = await values.youtube_notif_title_template.get_value(guild) or ''
+                    video_url = 'https://youtu.be/' + video_id
+                    content = string.Template(content_template).safe_substitute({
+                        'channel': video_channel, 'url': video_url,
+                    })
+                    await channel.send(content=content, user_mentions=True, role_mentions=True, mentions_everyone=True)
+                    logger.info('Sent youtube notification to guild %s channel %s', guild, channel_id)
+                else:
+                    logger.error('Did not send a youtube notification for guild %s channel %s: invalid channel ID',
+                                 guild, channel)
 
 
 @plugin.periodic_task(timedelta(minutes=10))
