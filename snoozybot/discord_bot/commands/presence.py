c350f0ea3f719f64bd65750deb1c41c72073b926
snazzyfox <12706268+snazzyfox@users.noreply.github.com>
2024-10-17 15:38:59 -0700

    default no ping on presence

--- snoozybot/discord_bot/commands/presence.py
+++ snoozybot/discord_bot/commands/presence.py
@@ -2,12 +2,14 @@ import logging
 import string
 
 import hikari
+from cachetools import TTLCache
 
 from snoozybot.config import values
 from snoozybot.utils import LightbulbPlugin
 
 plugin = LightbulbPlugin('presence')
 logger = logging.getLogger(__name__)
+_recent_notifs: TTLCache = TTLCache(maxsize=256, ttl=600)
 
 
 @plugin.listener(hikari.GuildAvailableEvent)
@@ -32,15 +34,16 @@ async def _process_presence(presence: hikari.MemberPresence, member: hikari.Memb
     streams = [activity for activity in presence.activities
                if activity.type == hikari.ActivityType.STREAMING and activity.url]
     member = member or await presence.fetch_member()
+    cache_key = (presence.guild_id, member.id)
     if streams and streaming_role not in member.role_ids:
         # add role
         await member.add_role(streaming_role)
         logger.info(f'Set streaming role for user {member} in guild {member.guild_id}.')
         # send message
         channel_id = await values.presence_streaming_notif_channel_id.get_value(presence.guild_id)
-        if channel_id:
+        if channel_id and cache_key not in _recent_notifs:
             template = await values.presence_streaming_notif_title_template.get_value(presence.guild_id) or ''
-            text = string.Template(template).safe_substitute({'user': member.mention})
+            text = string.Template(template).safe_substitute({'mention': member.mention, 'user': member.display_name})
             text_content, embeds = await _get_presence_message(streams)
             channel: hikari.TextableGuildChannel = member.get_guild().get_channel(channel_id)
             await channel.send(content=text + '\n' + text_content, embeds=embeds)
@@ -49,6 +52,7 @@ async def _process_presence(presence: hikari.MemberPresence, member: hikari.Memb
         # remove role
         await member.remove_role(streaming_role)
         logger.info(f'Removed streaming role for user {member} in guild {member.guild_id}')
+    _recent_notifs[cache_key] = 0  # value doesnt matter
 
 
 async def _get_presence_message(streams: list[hikari.RichActivity]) -> tuple[str, list[hikari.Embed]]:
